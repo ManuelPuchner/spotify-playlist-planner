ea4ad9c48f344d6be2ba1d0487799625ea246711
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2025-03-03 12:16:45 +0100

    fixed neon

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(neon);
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+export const prisma = new PrismaClient({ adapter });
